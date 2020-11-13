59489ac53dee5a0fcfa313295d9e3175de9ed2b2
Joao Faro <joao.faro@quicard.com.br>
2020-11-13 12:48:41 -0300

    first config

---
+++ src/main/java/br/eng/joaofaro/taskmanager/TaskManagerApplication.java
@@ -0,0 +1,13 @@
+package br.eng.joaofaro.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagerApplication.class, args);
+    }
+
+}
