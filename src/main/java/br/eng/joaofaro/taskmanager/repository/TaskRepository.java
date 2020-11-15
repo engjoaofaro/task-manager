5edbddd0961a53556daac6cda318e1616996a774
Joao Faro <joao.faro@quicard.com.br>
2020-11-15 19:28:36 -0300

    fix list

--- src/main/java/br/eng/joaofaro/taskmanager/repository/TaskRepository.java
+++ src/main/java/br/eng/joaofaro/taskmanager/repository/TaskRepository.java
@@ -4,6 +4,8 @@ import br.eng.joaofaro.taskmanager.entity.AccountUser;
 import br.eng.joaofaro.taskmanager.entity.Task;
 import br.eng.joaofaro.taskmanager.enums.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -16,9 +18,12 @@ import java.util.Optional;
 public interface TaskRepository extends JpaRepository<Task, Long> {
     Optional<Task> findById(Long id);
     Optional<Task> findByIdAndUser(Long id, AccountUser user);
-    List<Task> findAllByStatusAndUser(TaskStatus status, AccountUser user);
-    List<Task> findAllByStatus(TaskStatus status);
-    List<Task> findAllByUser(AccountUser user);
+    @Query("select t from Task t where t.status = :status and t.user = :user order by t.status asc")
+    List<Task> findAllByStatusAndUser(@Param(value = "status") TaskStatus status, @Param(value = "user") AccountUser user);
+    @Query("select t from Task t where t.status = :status order by t.status asc")
+    List<Task> findAllByStatus(@Param(value = "status") TaskStatus status);
+    @Query("select t from Task t where t.user = :user order by t.status asc")
+    List<Task> findAllByUser(@Param(value = "user") AccountUser user);
     @Transactional
     void deleteByIdAndUser(Long id, AccountUser user);
 }
